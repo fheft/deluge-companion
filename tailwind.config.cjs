25eeeb6bdf0d5382e0f4c65e73a5932f6a0eb394
Florian Heft <flo@fam-heft.de>
2023-11-14 23:12:54 +0100

    build: add tailwindcss
    * basic setup using svelte-add

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config}*/
+const config = {
+  content: ["./src/**/*.{ts,js,svelte,ts}", "./index.html"],
+
+  theme: {
+    extend: {},
+  },
+
+  plugins: [],
+};
+
+module.exports = config;
