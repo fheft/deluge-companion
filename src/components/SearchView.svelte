af4f7b9c96b00fd579786721a0cad866ab2e008a
Florian Heft <flo@fam-heft.de>
2024-10-01 21:50:05 +0200

    feat: add search clear button

--- src/components/SearchView.svelte
+++ src/components/SearchView.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import Cross from "../icons/Cross.svelte";
 
   export let input = "";
 
   let inputEl: HTMLInputElement;
+  $: hasContent = input.length > 0;
 
   onMount(() => {
     inputEl.focus();
   });
 
+  function clear() {
+    input = "";
+    inputEl.blur();
+  }
+
   function handleInputKeyDown(ev: KeyboardEvent) {
     ev.stopPropagation();
     if (ev.key === "Escape") {
-      input = "";
-      inputEl.blur();
+      clear();
     } else if (ev.key === "Enter") {
       inputEl.blur();
     }
@@ -26,13 +32,27 @@
   }
 </script>
 
-<input
-  type="search"
-  placeholder="Search... (⌨&#xFE0E; F)"
-  class="block w-full rounded-full bg-neutral-900 px-6 py-3 text-neutral-200 outline outline-1 outline-neutral-600 focus:bg-neutral-800 focus:outline-2"
-  bind:value={input}
-  bind:this={inputEl}
-  on:keydown={handleInputKeyDown}
-/>
+<div class="relative block">
+  <input
+    type="search"
+    placeholder="Search... (⌨&#xFE0E; F)"
+    class=" w-full rounded-full bg-neutral-900 px-6 py-3 text-neutral-200 outline outline-1 outline-neutral-600 focus:bg-neutral-800 focus:outline-2"
+    bind:value={input}
+    bind:this={inputEl}
+    on:keydown={handleInputKeyDown}
+  />
+  {#if hasContent}
+    <div
+      class="absolute right-0 top-0 flex aspect-square h-full items-center justify-center"
+    >
+      <button
+        on:click={clear}
+        class=" aspect-square h-8 rounded-full p-2 text-neutral-300 hover:bg-neutral-50/10"
+      >
+        <Cross />
+      </button>
+    </div>
+  {/if}
+</div>
 
 <svelte:window on:keydown={handleGlobalKeyDown} />
