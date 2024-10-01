4941ec7afb86dddca91481ba75dfc3ca78aba027
Florian Heft <flo@fam-heft.de>
2024-10-01 16:02:54 +0200

    feat: add search shortcuts (global and during input)

--- src/components/SearchView.svelte
+++ src/components/SearchView.svelte
@@ -8,12 +8,31 @@
   onMount(() => {
     inputEl.focus();
   });
+
+  function handleInputKeyDown(ev: KeyboardEvent) {
+    ev.stopPropagation();
+    if (ev.key === "Escape") {
+      input = "";
+      inputEl.blur();
+    } else if (ev.key === "Enter") {
+      inputEl.blur();
+    }
+  }
+
+  function handleGlobalKeyDown(ev: KeyboardEvent) {
+    if (["f", "F"].includes(ev.key)) {
+      inputEl.focus();
+    }
+  }
 </script>
 
 <input
   type="search"
-  placeholder="Search..."
+  placeholder="Search... (⌨&#xFE0E; F)"
   class="px-6 py-3 rounded-full block w-full bg-neutral-800 focus:bg-neutral-700 text-neutral-200 outline-neutral-300 focus:outline-2 outline-1 outline"
   bind:value={input}
   bind:this={inputEl}
+  on:keydown={handleInputKeyDown}
 />
+
+<svelte:window on:keydown|preventDefault={handleGlobalKeyDown} />
