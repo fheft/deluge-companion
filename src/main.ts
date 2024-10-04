9f94f4c87757e3cac0162786d3982269dcc14873
Florian Heft <flo@fam-heft.de>
2024-10-04 13:55:35 +0200

    chore: minor code cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./app.postcss";
 import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById("app"),
+  target: document.getElementById("app") as HTMLElement,
 });
 
 export default app;
